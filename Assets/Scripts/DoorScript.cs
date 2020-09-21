48d09f7d69b6f4244a9ecb47e576bffcc9994a3b
Justin <justintrupiano@gmail.com>
2020-09-21 14:08:54 -0600

    Initial commit

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    // public vars are accessible in the editor and other scripts
+    public float speed;
+    public Transform openTransform;
+    public Transform closedTransform;
+
+    // private vars are only accessible localy
+    private Transform currentTarget;
+
+    // Start is called before the first frame update
+    void Start(){
+      // Start with the door closed
+      currentTarget = closedTransform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // Rotate the door
+      RotateDoor();
+
+      // Check if the Space key is pressed
+      if (Input.GetKeyUp(KeyCode.Space)){
+        // If so, switch the currentTarget
+        SwitchTarget();
+      }
+    }
+
+
+    private void RotateDoor(){
+      // Only run if the currentTarget and the transform are not equal
+      if (currentTarget.rotation != transform.rotation){
+        // Using the Quaternion method RotateTowards, change the transform to get closer to the target transform.
+        transform.rotation = Quaternion.RotateTowards(transform.localRotation, currentTarget.rotation, speed * Time.deltaTime);
+      }
+    }
+
+    private void SwitchTarget(){
+      // Switch the currentTarget transform.
+      currentTarget = (currentTarget == openTransform) ? closedTransform : openTransform;
+    }
+}
