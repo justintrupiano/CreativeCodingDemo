0dc538809a200e5a52a0d6cf8c2d8d5fbf0d50ef
Justin <justintrupiano@gmail.com>
2020-09-25 17:48:55 -0600

    Added Scripts..

---
+++ Assets/Scripts/CharacterMovementScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovementScript : MonoBehaviour
+{
+    string name = "Bran";
+
+    public Vector3 velocity = new Vector3(0, 0, 0);
+
+    [Range(0, 10)]
+    public float speed;
+    private Rigidbody rigidbody;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKey(KeyCode.W))
+        velocity = new Vector3(speed, 0, 0);
+
+      if (Input.GetKey(KeyCode.A))
+        velocity = new Vector3(0, 0, speed);
+
+      if (Input.GetKey(KeyCode.S))
+        velocity = new Vector3(-speed, 0, 0);
+
+      if (Input.GetKey(KeyCode.D))
+        velocity = new Vector3(0, 0, -speed);
+
+      rigidbody.AddForce(velocity);
+
+      velocity = new Vector3(0, 0, 0);
+
+
+
+    }
+}
